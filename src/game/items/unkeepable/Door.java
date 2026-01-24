5d9be341f650b27cc0ae5f6f92dcf5c9bdba5c31
Maksym Kulynych <kulinichmax16082009@gmail.com>
2026-01-24 14:22:29 +0100

    Realizace proměnných třídy Door + gettery, settery, toString a konstruktor.

--- src/game/items/unkeepable/Door.java
+++ src/game/items/unkeepable/Door.java
@@ -1,14 +1,54 @@
 package game.items.unkeepable;
 
 import game.characters.Player;
+import game.gameUtils.Room;
 import game.items.Item;
 import game.uiUtils.RandomGenerator;
 
 public class Door extends Item {
-    public Door(String name) {
+    private Room nextRoom;
+    private Door nextDoor;
+    private boolean isOpen;
+
+    public Door(String name, Room nextRoom, Door nextDoor, boolean isOpen) {
         super(name);
+        this.nextRoom = nextRoom;
+        this.nextDoor = nextDoor;
+        this.isOpen = isOpen;
     }
 
+    public Room getNextRoom() {
+        return nextRoom;
+    }
+
+    public void setNextRoom(Room nextRoom) {
+        this.nextRoom = nextRoom;
+    }
+
+    public Door getNextDoor() {
+        return nextDoor;
+    }
+
+    public void setNextDoor(Door nextDoor) {
+        this.nextDoor = nextDoor;
+    }
+
+    public boolean isOpen() {
+        return isOpen;
+    }
+
+    public void setOpen(boolean open) {
+        isOpen = open;
+    }
+
+    @Override
+    public String toString() {
+        return "next room: " + nextRoom +
+                ", next door: " + nextDoor +
+                ", is open: " + isOpen;
+    }
+
+    //TODO: Implement useAbility method
     @Override
     public void useAbility(Player player, RandomGenerator rnd) {
     }
