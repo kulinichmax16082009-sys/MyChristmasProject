d0aa7c607d89ebe50c52839ef6e617eef70ad261
Maksym Kulynych <kulinichmax16082009@gmail.com>
2026-01-28 13:28:26 +0100

    Realizace metod třídy OutputUtils.

--- src/game/uiUtils/OutputUtils.java
+++ src/game/uiUtils/OutputUtils.java
@@ -1,8 +1,22 @@
 package game.uiUtils;
 
 import game.characters.Player;
+import game.gameUtils.Coordinates;
 
 public class OutputUtils {
-    public void printMessage(String message) {}
-    public void printCurrentRoom(Player player) {}
+    public void printMessage(String message) {
+        System.out.println(message);
+    }
+
+    public void printCurrentRoom(Player player) {
+        for (int i = player.getCurrentRoom().getHeight() - 1; i >= 0; i--) {
+            for (int j = 0; j < player.getCurrentRoom().getWidth(); j++) {
+                Coordinates coordinates = new Coordinates(j, i);
+                if (player.getCurrentRoom().getGameObjects().containsKey(coordinates)) System.out.print("| " + player.getCurrentRoom().getGameObjects().get(coordinates).getSprite() + " |");
+                else System.out.print("|    |");
+            }
+            System.out.println();
+        }
+    }
+
 }
