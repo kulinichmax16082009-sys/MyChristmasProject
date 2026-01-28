3918f9b3ab518823b2a4891483c20ccfdbfaa4bf
Maksym Kulynych <kulinichmax16082009@gmail.com>
2026-01-28 20:35:56 +0100

    Další pokus o randomné generování učeben ve škole. Byla přidána metoda clearWayFromDoorToTeacher();

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import game.characters.teachers.ItTeacher;
+import game.gameUtils.Coordinates;
 import game.gameUtils.Game;
 import game.gameUtils.Room;
 import game.gameUtils.RoomFactory;
@@ -6,19 +8,44 @@ import game.uiUtils.RandomGenerator;
 
 public class Main {
     public static void main(String[] args) {
-        OutputUtils outputUtils = new OutputUtils();
-        RoomFactory roomFactory = new RoomFactory();
+//        OutputUtils outputUtils = new OutputUtils();
+//        RoomFactory roomFactory = new RoomFactory();
 //        Room roomA = new Room("d", 10, 4);
 //        Room roomB = new Room("b", 6, 6);
-
-//        roomFactory.connectRooms(roomA, roomB);
+//        roomFactory.generateItems(roomA, new RandomGenerator());
+//        roomFactory.generateItems(roomB, new RandomGenerator());
 //
-//        System.out.println("Room A:");
-//        System.out.println(roomA.getGameObjects().get(new Coordinates(0,0)));
+//
+//        roomFactory.connectRooms(roomA, roomB, new RandomGenerator());
+//
+//        System.out.println("Connected Rooms:");
+//        outputUtils.printRoom(roomA);
+//        System.out.println();
+//        outputUtils.printRoom(roomB);
+//        System.out.println();
 
-        Room room = roomFactory.generateRoom(new RandomGenerator());
-        outputUtils.printRoom(room);
-        System.out.println(room.getName());
+//        Room room = roomFactory.generateRoom(new RandomGenerator());
+//        outputUtils.printRoom(room);
+//        System.out.println(room.getName());
+//
+//        roomFactory.connectRooms(roomA, roomB, new RandomGenerator());
+//        roomA.place(new Coordinates(roomA.getWidth() - 1,0), new ItTeacher(""));
+//        roomB.place(new Coordinates(roomB.getWidth() - 1,0), new ItTeacher(""));
+//
+//
+//        outputUtils.printRoom(roomA);
+//        System.out.println();
+//        outputUtils.printRoom(roomB);
+//        roomFactory.generateItems(roomA, new RandomGenerator());
+//        roomFactory.generateItems(roomB, new RandomGenerator());
+//        System.out.println("After generating items:");
+//        outputUtils.printRoom(roomA);
+//        System.out.println();
+//        outputUtils.printRoom(roomB);
+//
+//        roomFactory.clearWayFromDoorToTeacher(roomB);
+//        System.out.println("After clearing way in room B:");
+//        outputUtils.printRoom(roomB);
 
         Game game = new Game();
         game.play();
--- src/game/gameUtils/RoomFactory.java
+++ src/game/gameUtils/RoomFactory.java
@@ -21,7 +21,7 @@ public class RoomFactory {
         allPossibleItems = new ArrayList<>();
     }
 
-    //TODO: Učit jména učeben z externího souboru + přidat ošetření cesty od učitele k dveřim
+    //TODO: Učit jména učeben z externího souboru (nebo generovat podle nějakého vzoru)
     public Room generateRoom(RandomGenerator rnd) {
         Room room = new Room("Učebna č." + rnd.randomNumber(1, 100), rnd.randomNumber(2, 15), rnd.randomNumber(2, 15));
         room.place(new Coordinates(rnd.randomNumber(0, room.getWidth() - 1), rnd.randomNumber(0, room.getHeight() - 1)), Teacher.teacherFactory(rnd.randomNumber(1,5)));
@@ -30,7 +30,7 @@ public class RoomFactory {
     }
 
     //TODO: Ošetřít podmínku při které není možné dveře umístit
-    public void connectRooms(Room roomA, Room roomB) {
+    public void connectRooms(Room roomA, Room roomB, RandomGenerator rnd) {
         Door doorA = new Door();
         Door doorB = new Door();
 
@@ -39,13 +39,21 @@ public class RoomFactory {
         doorB.setNextRoom(roomA);
         doorB.setNextDoor(doorA);
 
-        for (int i = 0; i < roomA.findFreeCoordinates().size(); i++) {
-            boolean isPlaced = roomA.place(roomA.findFreeCoordinates().get(i), doorA);
-            if (isPlaced) break;
+        boolean isPlaced = false;
+        while (!isPlaced) isPlaced = roomA.place(new Coordinates(rnd.randomNumber(0, roomA.getWidth() - 1), rnd.randomNumber(0, roomA.getHeight() - 1)), doorA);
+        isPlaced = false;
+        while (!isPlaced) isPlaced = roomB.place(new Coordinates(rnd.randomNumber(0, roomB.getWidth() - 1), rnd.randomNumber(0, roomB.getHeight() - 1)), doorB);
+
+        for (int i = 0; i < 7; i++) {
+            if (!doorA.isAnyObjectNearByType(Teacher.class, false)) {
+                roomA.getGameObjects().remove(doorA.getNearObjectByType(GameObject.class, false).getCoordinates());
+            }
         }
-        for (int i = 0; i < roomB.findFreeCoordinates().size(); i++) {
-            boolean isPlaced = roomB.place(roomB.findFreeCoordinates().get(i), doorB);
-            if (isPlaced) break;
+
+        for (int i = 0; i < 7; i++) {
+            if (!doorB.isAnyObjectNearByType(Teacher.class, false)) {
+                roomB.getGameObjects().remove(doorB.getNearObjectByType(GameObject.class, false).getCoordinates());
+            }
         }
     }
 
@@ -54,13 +62,14 @@ public class RoomFactory {
         for (int i = 0; i < room.getHeight(); i++) {
             for (int j = 0; j < room.getWidth(); j++) {
                 for (Item possibleItem : allPossibleItems) {
-                    if (rnd.generateProbability(possibleItem.getSpawnChance()) && !possibleItem.isAnyObjectNearByType(Item.class, true)) {
+                    if (rnd.generateProbability(possibleItem.getSpawnChance()) && !possibleItem.isAnyObjectNearByType(GameObject.class, false)) {
                         room.place(new Coordinates(j, i), possibleItem);
                     }
                 }
             }
         }
     }
+
     public void initializeAllPossibleItems() {
         allPossibleItems.add(new Chair().initializeItem());
         allPossibleItems.add(new Wall().initializeItem());
@@ -69,5 +78,35 @@ public class RoomFactory {
         allPossibleItems.add(new EnergyDrink().initializeItem());
         allPossibleItems.add(new GoldenKey().initializeItem());
         allPossibleItems.add(new MagicPear().initializeItem());
+    }
+
+    public void clearWayFromDoorToTeacher(Room room) {
+        int coordinatesDiffX;
+        int coordinatesDiffY;
+
+        Teacher teacher = null;
+        Door door = null;
+
+        for (GameObject gameObject : room.getGameObjects().values()) {
+            if (gameObject instanceof Door) door = (Door) gameObject;
+            if (gameObject instanceof Teacher) teacher = (Teacher) gameObject;
+        }
+
+        if (teacher == null || door == null) return;
+
+        Coordinates doorCoordinates = door.getCoordinates();
+
+        coordinatesDiffX = teacher.getCoordinates().getX() - door.getCoordinates().getX();
+        coordinatesDiffY = teacher.getCoordinates().getY() - door.getCoordinates().getY();
+
+        //X axis
+        for (int i = 1; i < Math.abs(coordinatesDiffX); i++) {
+            room.getGameObjects().remove(new Coordinates(doorCoordinates.getX() + Integer.signum(coordinatesDiffX) * i, doorCoordinates.getY()));
+        }
+
+        //Y axis
+        for (int i = 0; i < Math.abs(coordinatesDiffY); i++) {
+            room.getGameObjects().remove(new Coordinates(doorCoordinates.getX() + coordinatesDiffX, doorCoordinates.getY() + Integer.signum(coordinatesDiffY) * i));
+        }
     }
 }
