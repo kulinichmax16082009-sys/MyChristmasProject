40c0902f281026e1a0ba1a2633271e52b0033eed
Maksym Kulynych <kulinichmax16082009@gmail.com>
2026-01-31 20:49:42 +0100

    Realizace proměnných třídy Task a všech její metod.

--- src/game/inventories/Task.java
+++ src/game/inventories/Task.java
@@ -1,8 +1,61 @@
 package game.inventories;
 
 public class Task {
-    public void startTimer() {}
+    private String description;
+    private String answer;
+    private long duration;
+
+    public Task() {
+        description = "";
+        duration = 0;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public long getDuration() {
+        return duration;
+    }
+
+    public void setDuration(long duration) {
+        this.duration = duration;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public void addDuration(long amount) {
+        duration += amount;
+    }
+
+    public void startTimer() {
+        try {
+            while (!isExpired()) {
+                duration--;
+                Thread.sleep(1000);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public boolean isExpired() {
-        return false;
+        return duration <= 0;
+    }
+
+    @Override
+    public String toString() {
+        return "popis: " + description +
+                ", trvání: " + duration + " s";
     }
 }
